a366e280492cb67e3b2d2fbdab034c70b39068b0
YoonmaYer <rkddbs0813@gmail.com>
2024-01-06 19:19:22 +0900

    final

---
+++ src/final1/ConstructInit.java
@@ -0,0 +1,11 @@
+package final1;
+
+public class ConstructInit {
+
+    final int value;
+
+    //생성자를 이용한 초기화, 위처럼 값이 할당되지 않았다면 가능
+    public ConstructInit(int value) {
+        this.value = value;
+    }
+}
---
+++ src/final1/FieldInit.java
@@ -0,0 +1,14 @@
+package final1;
+
+public class FieldInit {
+
+    static final int CONST_VALUE = 10;
+    final int value = 10;
+
+    /*
+    //위에서 이미 할당이 되었기 때문에 생성자로 초기화 불가능
+    public FieldInit(int value) {
+        this.value = value;
+       }
+     */
+}
