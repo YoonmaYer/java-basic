a863efc52a838c4e3dac2552d6447a2d0a9689b1
kang yoon <rkddbs0813@gmail.com>
2024-01-08 11:46:28 +0900

    Extends

--- src/extends1/ex/Item.java
+++ src/extends1/ex/Item.java
@@ -1,6 +1,20 @@
 package extends1.ex;
 
 public class Item {
-    public static String name;
-    public static int price;
+    private final String name;
+    private final int price;
+
+  public Item(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+
+    public void print() {
+        System.out.println("이름:" + name + ", 가격:" + price);
+    }
 }
